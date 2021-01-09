2bf5f1d46742e0e1da4fa13db8b73127aa7da775
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-09 22:03:04 +0300

    Added one lifecycle method

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -7,6 +7,12 @@ class ErrorBoundary extends React.Component {
       hasError: false,
     };
   }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
   render() {
     return <div></div>;
   }
