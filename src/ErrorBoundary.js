dede74442db0f8c90981786a2b676b87abab9e25
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-09 22:01:07 +0300

    Added error boundary constructor

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
   render() {
     return <div></div>;
   }
